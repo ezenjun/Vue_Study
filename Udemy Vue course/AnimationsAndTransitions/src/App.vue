d99368f78c347825333801863e479c1e820c6e24
Ezenjun <ezenjun07@gmail.com>
2021-08-24 23:49:33 +0900

    animation css

---
+++ Udemy Vue course/AnimationsAndTransitions/src/App.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="container">
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock" >Animate</button>
+  </div>
+  <div class="container">
+    <transition name="para">
+      <p v-if="paraisVisible">this is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">toggle paragraph</button>
+  </div>
+  <transition name="modal">
+    <base-modal @close="hideDialog" v-if="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
+  </transition>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+  
+</template>  
+
+<script>
+export default {
+  data() {
+    return { 
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraisVisible: false,
+    };
+  },
+  methods: {
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+    animateBlock(){
+      this.animatedBlock = !this.animatedBlock;
+    },
+    toggleParagraph(){
+      this.paraisVisible = !this.paraisVisible;
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+  
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+
+.animate{
+  /* transform: translateX(-150px); */
+  animation: slide-fade 0.3s ease-out forwards;
+}
+
+.para-enter-from{
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+.para-enter-active{
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-enter-t0{
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+/* .v-leave-from{opacity: 1;
+  transform: translateY(0);} */
+.para-leave-active{
+  animation: slide-scale 0.3s ease-out;
+}
+/* .v-leave-to{opacity: 0;
+  transform: translateY(-30px);} */
+
+.modal-enter-from{
+
+}
+.modal-enter-active{
+  animation: modal 0.3s ease-out;
+}
+.modal-enter-to{
+
+}
+
+@keyframes modal {
+  from{
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+  to{
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+}
+
+
+@keyframes slide-fade{
+  0%{
+    transform: translateX(0) scale(1);
+  }
+
+  70%{
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100%{
+    transform: translateX(-150px) scale(1);
+  }
+}
+
+@keyframes slide-scale{
+  0%{
+    transform: translateX(0) scale(1);
+  }
+
+  70%{
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100%{
+    transform: translateX(-150px) scale(1);
+  }
+}
+</style>
